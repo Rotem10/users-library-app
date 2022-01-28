6d502fbd38cbc192d5cfd7d060bdc863a06fd031
Rotem10 <rotem200191@gmail.com>
2022-01-28 02:09:27 +0200

    user item complete

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -10,12 +10,14 @@ const UserItem = ({ users }) => {
     <div className='accordion'>
       {users.map((user, index) => {
         let title = `${user.name.title}. ${user.name.first} ${user.name.last}`;
+        let location = `${user.location.country}, ${user.location.city}, ${user.location.street.name} ${user.location.street.number}`;
         return (
           <div className='accordion-item' key={index}>
             <h2 className='accordion-header' id='headingOne'>
+              {" "}
               <button
                 className={`accordion-button  ${
-                  index === activeI ? "show" : "collapsed"
+                  index === activeI ? "show fw-bold" : "collapsed"
                 }`}
                 type='button'
                 data-bs-toggle='collapse'
@@ -29,11 +31,34 @@ const UserItem = ({ users }) => {
               </button>
             </h2>
             <div
-              id='collapseOne'
-              className='accordion-collapse collapse collapse'
+              className={`accordion-collapse collapse  ${
+                index === activeI ? "show" : "collapse"
+              }`}
               aria-labelledby='headingOne'
               data-bs-parent='#accordionExample'>
-              <div className='accordion-body'></div>
+              <div className='accordion-body'>
+                <table className='table'>
+                  <thead>
+                    <tr>
+                      <th scope='col'>ID</th>
+                      <th scope='col'>Picture</th>
+                      <th scope='col'>Email</th>
+                      <th scope='col'>Location</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className='align-baseline'>
+                      <th scope='row'>{user.id.value}</th>
+                      <td>
+                        {" "}
+                        <img src={user.picture.medium} alt='user-img' />
+                      </td>
+                      <td>{user.email}</td>
+                      <td>{location}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         );
